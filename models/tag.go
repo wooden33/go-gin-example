b3d9f4fc65e6fde295f9db1f372c5f58acb9bddf
jhliu <jhliucs@163.com>
2024-08-07 12:00:22 +0800

    feat: Tag接口全部实现

--- models/tag.go
+++ models/tag.go
@@ -1,6 +1,11 @@
 package models
 
-import "log"
+import (
+	"log"
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Tag struct {
 	Model
@@ -24,4 +29,57 @@ func GetTagTotal(maps any) (count int) {
 	db.Model(&Tag{}).Where(maps).Count(&count)
 
 	return
+}
+
+func ExistTagByID(id int) bool {
+	var tag Tag
+	db.Select("id").Where("id = ?", id).First(&tag)
+	if tag.ID > 0 {
+		return true
+	}
+
+	return false
+}
+
+func ExistTagByName(name string) bool {
+	var tag Tag
+	db.Select("id").Where("name = ?", name).First(&tag)
+	if tag.ID > 0 {
+		return true
+	}
+	return false
+}
+
+func AddTag(name string, state int, createdBy string) bool {
+	db.Create(&Tag{
+		Name:      name,
+		State:     state,
+		CreatedBy: createdBy,
+	})
+	return true
+}
+
+func EditTag(id int, data any) bool {
+	db.Model(&Tag{}).Where("id = ?", id).Update(data)
+
+	return true
+}
+
+func DeleteTag(id int) bool {
+	db.Where("id = ?", id).Delete(&Tag{})
+
+	return true
+}
+
+// gorm的回调方法
+func (tag *Tag) BeforeCreate(scope *gorm.Scope) error {
+	scope.SetColumn("CreatedOn", time.Now().Unix())
+
+	return nil
+}
+
+func (tag *Tag) BeforeUpdate(scope *gorm.Scope) error {
+	scope.SetColumn("ModifiedOn", time.Now().Unix())
+
+	return nil
 }
